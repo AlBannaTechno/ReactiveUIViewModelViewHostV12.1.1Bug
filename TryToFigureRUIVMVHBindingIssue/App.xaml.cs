7d913832355ecda0046f1a7187b8c412e3b850dd
AlBannaTechno <Al_Banna_Techno@yahoo.com>
2020-10-24 16:10:06 +0200

    Init v11.5.3.5

---
+++ TryToFigureRUIVMVHBindingIssue/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using ReactiveUI;
+using Splat;
+
+namespace TryToFigureRUIVMVHBindingIssue
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+        }
+
+        private void App_OnStartup(object sender, StartupEventArgs e)
+        {
+            Locator.CurrentMutable.Register(() => new SubLayoutView(), typeof(IViewFor<SubLayoutViewModel>));
+
+            var mw = new MainWindow();
+            App.Current.MainWindow = mw;
+            mw.Show();
+        }
+    }
+}
---
+++ TryToFigureRUIVMVHBindingIssue/SubLayoutView.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ReactiveUI;
+
+namespace TryToFigureRUIVMVHBindingIssue
+{
+    /// <summary>
+    /// Interaction logic for SubLayoutView.xaml
+    /// </summary>
+    public partial class SubLayoutView : ReactiveUserControl<SubLayoutViewModel>
+    {
+        public SubLayoutView()
+        {
+            InitializeComponent();
+            this.WhenActivated(disposable =>
+            {
+                this.Bind(ViewModel, vm => vm.Text, v => v.TextBlock.Text).DisposeWith(disposable);
+            });
+        }
+    }
+}
